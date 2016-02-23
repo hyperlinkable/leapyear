c0464df0fe95de7968216f64c2b2603b8c0d47e6
David Wynn <davidwwynn@gmail.com>
2016-02-22 20:45:20 -0800

    add error states for NaN

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,51 @@
+var hasError;
+var errorText;
+
 var leapYear = function(year) {
-  if ((year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0)) {
+  if (isNaN(year)) {
+        hasError = true;
+        errorText = "Please enter a number";
+      } else if ((year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0)) {
+      hasError = false;
+      // alert("is a leap year");
     return true;
-  } else {
+    } else {
+    hasError = false;
     return false;
   }
 };
+
+// for testing
+// if (isNaN(parseInt(year)) && (year )) {
+//   alert("That's not a number");
+// };
+
+
+
+$(document).ready(function() {
+  $("form#leap-year").submit(function(event) {
+    var year = parseInt($("input#year").val());
+    var result = leapYear(year);
+
+    $(".year").text(year);
+
+    if (hasError === true) {
+      $(".errorText").text(errorText);
+      $("#error").show();
+      $("#resultText").hide();
+    } else
+    if (!result) {    // same as writing if (result === false)
+      $(".not").text("not");
+      $("#error").hide();
+      $("#resultText").show();
+    } else {
+      $(".not").text("");
+      $("#error").hide();
+      $("#resultText").show();
+    }
+
+    $("#response").show();
+
+    event.preventDefault();
+  });
+});
